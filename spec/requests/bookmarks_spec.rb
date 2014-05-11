8fdecab53dbcc2cbe6dfd087871f6ce3f5e9b18a
Jeremy Radant <radant@madisoncollege.edu>
2014-05-11 00:13:23 -0500

    Project 2 Everything

--- spec/requests/bookmarks_spec.rb
+++ spec/requests/bookmarks_spec.rb
@@ -3,9 +3,21 @@ require 'spec_helper'
 describe "Bookmarks" do
   describe "GET /bookmarks" do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get bookmarks_path
       response.status.should be(200)
     end
   end
+  describe "Signup page" do
+    it "should have the content 'Signup'" do
+      visit '/bookmarks/signup'
+      expect(page).to have_content('Signup')
+    end
+
+    it "should have the title 'Signup'" do
+      visit '/bookmarks/signup'
+      expect(page).to have_title("Super Bookmarks | Signup")
+    end
+  end
 end
+
+
